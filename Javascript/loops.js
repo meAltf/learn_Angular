7aecaef8b0642a49cac834e42c480d6b91a584b6
meAltf <alataf.dce.cs1@gmail.com>
2025-09-06 00:17:34 +0530

    loop-1

---
+++ Javascript/loops.js
@@ -0,0 +1,24 @@
+for(let i=1; i<=1000; i++){
+    console.log("Hey robert!");
+}
+
+// calculate sum
+let sum = 0;
+for(let i=0; i<=10;i++){
+    sum+= i;
+}
+console.log(sum);
+
+// while loop
+let i = 10;
+while(i <= 14){
+    console.log(`Increment the value: ${i}`);
+    i++;
+}
+
+
+// do-while loop
+let k = 20;
+do{
+    console.log("Atlest one time it will run even it's not satisfy the answer");
+} while(k <10);
