a9b128109144d8e472abbe203a3508ee04eda389
meAltf <alataf.dce.cs1@gmail.com>
2025-09-05 23:35:23 +0530

    Operator | required

---
+++ Javascript/operators.js
@@ -0,0 +1,16 @@
+// Arithmetic operator
+let a = 5;
+let b = 6;
+
+console.log(a+b);
+console.log(a ** 3); // 5^3
+
+// strict comparison operator
+let c = 10;
+let d = "10";
+
+console.log(c == d); // true | bcz it will check value only
+console.log(c != d); // false 
+
+console.log(c === d); // false | bcz it will check types and value both
+console.log(c !== d); // true
