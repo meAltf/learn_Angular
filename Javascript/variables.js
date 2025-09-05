e67eec10d70022a16a79cea9537b0db90a5057ac
meAltf <alataf.dce.cs1@gmail.com>
2025-09-05 22:25:00 +0530

    basic understanding of js | dynamically assigned var

---
+++ Javascript/variables.js
@@ -0,0 +1,18 @@
+// JS- Dynamically types language
+
+fullName = "Robert!";
+age = 23;
+salary = 140000000
+console.log(fullName + " " + age + " " + salary);
+
+// null -> a special value | khali
+x = null;
+console.log(x);
+
+// undefined -> malum hi nhi ki kya hai iske ander
+y = undefined
+console.log(y);
+
+isFollow = true;
+console.log(isFollow);
+
