82c97407090725f5a04a729872ee9c27e531ca8e
meAltf <alataf.dce.cs1@gmail.com>
2025-09-05 23:21:21 +0530

    Practice ques-1 | create object for a product on amazon

---
+++ Javascript/practiceQues/product.js
@@ -0,0 +1,10 @@
+const product = {
+    title: "Ball Pen parker",
+    rating: 4,
+    offer: 5,
+    price: 350
+};
+
+console.log(product);
+console.log(typeof(product));
+console.log(product.title + " " + product.rating + " " + product.price);
