53cf559f03fdee2f4c36436fbea2d3ff2778fdae
meAltf <alataf.dce.cs1@gmail.com>
2025-09-06 22:27:34 +0530

    Dom manipulation -1 | selecting with id,className,tagName

--- Javascript/DOM/script.js
+++ Javascript/DOM/script.js
@@ -1,1 +1,30 @@
 alert("Hey JS!")
+
+// window.document.body and document.body both are same bcz window is globally appearance
+console.dir(window.document.body);
+console.log(document.body);
+
+console.dir(document.body.childNodes[1]);
+console.dir(document.head);
+
+// Action
+document.body.style.background = "green";
+
+// document.body.childNodes[3].innerText = "Topic 1 : Hey started learning!";
+
+// DOM manipulation
+// #->id | #heading | it's always unique
+// 1. selecting with id, className, tag |  document.getElementById/getElementByClassName/getElementByTagName
+
+let heading = document.getElementById("heading"); // h1
+console.dir(heading);
+
+let headings = document.getElementsByClassName("heading-class")
+console.dir(headings);
+
+let parahs = document.getElementsByTagName("p");
+console.dir(parahs);
+
+
+
+
