6049495dbdb0bb8a4cc6cff4aa6338c672dd58c2
meAltf <alataf.dce.cs1@gmail.com>
2025-09-08 00:10:17 +0530

    promise chaining implemented

--- Javascript/AsyncProg/promises.js
+++ Javascript/AsyncProg/promises.js
@@ -63,6 +63,9 @@ promise.catch( ( err ) ) => { .... } )
 
 **/
 
+
+
+/**
 const getPromiseRes = () => {
     return new Promise((resolve, reject) => {
         console.log("I am a promise");
@@ -85,4 +88,33 @@ const getPromiseRej = () => {
 let promiseRej = getPromiseRej();
 promiseRej.catch((res) => {
     console.log("promise rejected", res);
+});
+
+**/
+
+// Promises chaining demo
+function asyncFunc1() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("data1");
+            resolve("success");
+        }, 4000);
+    });
+}
+
+function asyncFunc2() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("data2");
+            resolve("success");
+        }, 4000);
+    });
+}
+
+console.log("fetching data1...");
+let p1 = asyncFunc1();
+p1.then((res) => {
+    console.log("fetching data2...");
+    let p2 = asyncFunc2();
+    p2.then((res) => { });
 });
