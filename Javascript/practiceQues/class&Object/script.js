8767be719002f4b736c0179b92e15639b9773d70
meAltf <alataf.dce.cs1@gmail.com>
2025-09-07 22:07:52 +0530

    Admin & editData

--- Javascript/practiceQues/class&Object/script.js
+++ Javascript/practiceQues/class&Object/script.js
@@ -11,6 +11,13 @@ class User {
     }
 }
 
+class Admin extends User {
+
+    editData() {
+        DATA = "The secret infromation after edited by admin";
+    }
+}
+
 let student1 = new User("Robert", "robert@google.com");
 let student2 = new User("broon", "broon@amazon.com");
 
@@ -18,3 +25,8 @@ let teacher1 = new User("Alataf", "alataf@microsoft.com");
 
 console.log(student1);
 console.log(student2.viewData());
+
+let admin1 = new Admin("admin1", "admin1@gmail.com");
+console.log(admin1);
+admin1.editData();
+console.log(`After change: ${DATA}`);
