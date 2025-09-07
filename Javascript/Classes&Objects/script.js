50514ce3b4978d545eaa120f9b725a8489023d8f
meAltf <alataf.dce.cs1@gmail.com>
2025-09-07 12:11:56 +0530

    added prototypes in new object from old one

--- Javascript/Classes&Objects/script.js
+++ Javascript/Classes&Objects/script.js
@@ -13,9 +13,37 @@ const employee = {
     calcTax1() {
         console.log("Tax rate is 10% from 2nd way of creating function");
     },
-    calcTax2: function () {
-        console.log("Tax rate is 20% from normal way of creating function");
-    },
 };
 console.log(employee.calcTax1());
-console.log(employee.calcTax2());
+
+/**
+Prototypes in JS
+- JS objects have a special property called prototype.
+- A javaScript object is an entity having state and behavior (properties and method).
+
+-We can set prototype using[ __proto__ ]
+
+*If object & prototype have same method,
+object’s method will be used.
+
+**/
+
+
+const employee1 = {
+    salary: 900000,
+};
+const employee2 = {
+    salary: 900000,
+};
+const employee3 = {
+    salary: 900000,
+};
+const employee4 = {
+    salary: 900000,
+};
+
+// add prototype
+employee1.__proto__ = employee;
+employee2.__proto__ = employee;
+employee3.__proto__ = employee;
+employee4.__proto__ = employee;
