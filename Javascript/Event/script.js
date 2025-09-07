11136a339adac97ab4b535144ecec6092c87ba0b
meAltf <alataf.dce.cs1@gmail.com>
2025-09-07 11:09:22 +0530

    remove eventlistener

--- Javascript/Event/script.js
+++ Javascript/Event/script.js
@@ -36,8 +36,9 @@ div.onmouseover = (evt) => {
 
 
 // Event listener | node.addEventListener( event, callback ) | node.removeEventListener( event, callback ) 
-// Note : the callback reference should be same to remove
+// Note : the callback reference should be same to remove | while remove event it's most important part
 
+/**
 btn1.addEventListener("click", (evt) => {
     console.log("button1 was clicked");
     console.log(evt);
@@ -47,3 +48,27 @@ btn1.addEventListener("click", (evt) => {
 btn1.addEventListener("click", () => {
     console.log("button1 was clicked- handler2");
 });
+
+**/
+
+// add Event listesner demo
+btn1.addEventListener("click", () => {
+    console.log("button1 was clicked- handle1");
+});
+
+btn1.addEventListener("click", () => {
+    console.log("button1 was clicked- handle2");
+})
+
+const handler3 = () => {
+    console.log("button1 was clicked- handle3");
+};
+
+btn1.addEventListener("click", handler3);
+
+btn1.addEventListener("click", () => {
+    console.log("button1 was clicked- handle4");
+})
+
+// remove Event listener
+btn1.removeEventListener("click", handler3);
