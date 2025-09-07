db9da01704abf51acea695b6d512b9af532b16ee
meAltf <alataf.dce.cs1@gmail.com>
2025-09-07 10:17:15 +0530

    event object demo

--- Javascript/Event/script.js
+++ Javascript/Event/script.js
@@ -1,5 +1,6 @@
 let btn1 = document.querySelector("#btn1");
 
+/** 
 btn1.onclick = () => {
     console.log("btn1 was clicked");
     let num = 23;
@@ -11,3 +12,21 @@ let div = document.querySelector("div");
 div.onmouseover = () => {
     console.log("You're under radar😂");
 }
+
+**/
+
+// EVENT object | that contains all event related methods and properties
+btn1.onclick = (evt) => {
+    console.log(evt);
+    console.log(evt.type);
+    console.log(evt.target);
+    console.log(evt.clientX, evt.clientY);
+};
+
+let div = document.querySelector("div");
+div.onmouseover = (evt) => {
+    console.log(evt);
+    console.log(evt.type);
+    console.log(evt.target);
+    console.log(evt.clientX, evt.clientY);
+};
