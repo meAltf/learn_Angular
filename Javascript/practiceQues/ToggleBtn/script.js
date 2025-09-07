2b32c0f304d1dd5a1e75ade197b1b2e136c9c1f2
meAltf <alataf.dce.cs1@gmail.com>
2025-09-07 11:36:10 +0530

    corrected logic

--- Javascript/practiceQues/ToggleBtn/script.js
+++ Javascript/practiceQues/ToggleBtn/script.js
@@ -8,9 +8,11 @@ modebtn.addEventListener("click", () => {
     if (currMode === "light") {
         currMode = "dark";
         body.classList.add("dark");
+        body.classList.remove("light");
     } else {
         currMode = "light";
         body.classList.add("light");
+        body.classList.remove("dark");
     }
 
     console.log(currMode);
