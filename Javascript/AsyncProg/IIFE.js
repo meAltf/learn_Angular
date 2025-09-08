44b54bac77866896476aced9bebb24245110d4cc
meAltf <alataf.dce.cs1@gmail.com>
2025-09-08 08:52:26 +0530

    IIFE | theory

---
+++ Javascript/AsyncProg/IIFE.js
@@ -0,0 +1,20 @@
+/**
+
+IIFE : Immediately Invoked Function Expression
+
+- IIFE is a function that is called immediately as soon as it is defined.
+
+- (function () => {
+    //
+})();
+
+- (() => {
+    //
+})();
+
+- (async () => {
+    //
+})();
+
+**/
+
