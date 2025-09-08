72b5c8a493cddc1f8193b758711be876a8213f1f
meAltf <alataf.dce.cs1@gmail.com>
2025-09-08 23:19:26 +0530

    typescript compiler behavior

--- Typescript/Basics/index.js
+++ Typescript/Basics/index.js
@@ -1,1 +1,3 @@
 console.log('Hello Robert, from typescript');
+// variable typed
+var age = 20;
--- Typescript/Basics/index.ts
+++ Typescript/Basics/index.ts
@@ -1,1 +1,5 @@
 console.log('Hello Robert, from typescript');
+
+// variable typed
+let age: number  = 20;
+// age = "a";  | Not it will give error in typescript bcz we defined age as type of number so it can't take string or any other values.
