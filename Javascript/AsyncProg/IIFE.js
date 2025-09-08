83e7ba5203deaa8f2bff70e6c151dc1110800aae
meAltf <alataf.dce.cs1@gmail.com>
2025-09-08 08:54:28 +0530

    demo on IIFE with async func

--- Javascript/AsyncProg/IIFE.js
+++ Javascript/AsyncProg/IIFE.js
@@ -18,3 +18,21 @@ IIFE : Immediately Invoked Function Expression
 
 **/
 
+function api() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("weather data..");
+            resolve(200);
+        }, 2000);
+    });
+}
+
+
+(async function getWeatherData() {
+    await api();
+    await api();
+    await api();
+    await api();
+})();
+
+//console.log(getWeatherData());
