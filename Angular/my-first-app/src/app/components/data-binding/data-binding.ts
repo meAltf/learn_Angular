a72723ac7a0cc214dc50a0eb35ec99bc26e6c2b9
meAltf <alataf.dce.cs1@gmail.com>
2025-09-14 00:17:11 +0530

    tried two way data binding with ngModel | didn't work

--- Angular/my-first-app/src/app/components/data-binding/data-binding.ts
+++ Angular/my-first-app/src/app/components/data-binding/data-binding.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-data-binding',
-  imports: [],
+  standalone: true,   
+  imports: [FormsModule],
   templateUrl: './data-binding.html',
   styleUrl: './data-binding.css'
 })
