f768b65922862656591d66ef037e7c5169031bc9
meAltf <alataf.dce.cs1@gmail.com>
2025-09-13 21:32:01 +0530

    OnInit | ngAfterVuewInit | ngOnDestroy understanding

--- Angular/my-first-app/src/app/components/admin/admin.ts
+++ Angular/my-first-app/src/app/components/admin/admin.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-admin',
@@ -6,6 +6,26 @@ import { Component } from '@angular/core';
   templateUrl: './admin.html',
   styleUrl: './admin.css'
 })
-export class Admin {
+export class Admin implements OnInit, AfterViewInit, OnDestroy{
+
+  constructor(){
+    console.log('constructor executed!')
+    // properties initialization
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit life cycle.')
+      // api calls
+      // subscription
+  }
+
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit life cycle')
+  }
+
+  ngOnDestroy(): void {
+    console.log('onDestroy life cycle')
+  }
+
 
 }
